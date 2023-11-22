09f9fafe6c152efae998eb2c0a7a10e993689ed1
mgrevious <monica.grevious@gmail.coml>
2023-11-22 01:29:04 -0500

    Added event handlers for mobile menu.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './style.css';
+import "./style.css";
+
+(() => {
+  const menuIconBtn = document.getElementById("icon-menu-btn");
+  const closeBtn = document.getElementById("close-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuIconBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    mobileMenu.className = "block";
+  });
+
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    mobileMenu.className = "hidden";
+  });
+})();

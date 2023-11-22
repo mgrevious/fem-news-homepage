af423da3e11b906dc58b160d02879fa01f58e312
mgrevious <monica.grevious@gmail.coml>
2023-11-21 23:18:52 -0500

    Mobile styling is complete. Working on desktop.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: ["./dist/*.{html,js}"],
@@ -9,8 +10,30 @@ module.exports = {
       spacing: {
         128: "32rem",
       },
+      fontFamily: ["Inter", ...defaultTheme.fontFamily.sans],
       fontSize: { "3xl": "2rem" },
+      colors: {
+        header: "#00001A",
+        primary: "#5E607A",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        p: {
+          color: theme("colors.primary"),
+          fontSize: "15px",
+          lineHeight: "26px",
+          fontWeight: theme("fontWeight.light"),
+        },
+        h2: {
+          fontSize: theme("fontSize.xl"),
+          fontWeight: theme("fontWeight.bold"),
+          lineHeight: "24px",
+          marginBottom: "8px",
+        },
+      });
+    }),
+  ],
 };

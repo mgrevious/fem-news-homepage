1db6c70e5d9e389e4adf46fd52c43798424adf1e
mgrevious <monica.grevious@gmail.coml>
2023-11-22 19:30:45 -0500

    Animated slide out menu is now working.

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,22 @@ import "./style.css";
     "icon-menu-btn"
   ) as HTMLButtonElement;
   const closeBtn = document.getElementById("close-btn") as HTMLButtonElement;
-  const mobileMenu = document.getElementById("menu") as HTMLButtonElement;
+  const menuBg = document.getElementById("menu-bg") as HTMLButtonElement;
+  const menu = document.querySelector("#menu > nav") as HTMLButtonElement;
 
   menuIconBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    mobileMenu.className = "block";
+    menu.classList.remove("-right-2/3");
+    menu.classList.add("right-0");
+    menuBg.classList.remove("hidden");
+    menuBg.classList.add("block");
   });
 
   closeBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    mobileMenu.className = "hidden";
+    menu.classList.add("-right-2/3");
+    menu.classList.remove("right-0");
+    menuBg.classList.remove("block");
+    menuBg.classList.add("hidden");
   });
 })();
